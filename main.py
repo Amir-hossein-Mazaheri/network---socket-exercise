e94a52629e67e71ea0d1131f9e4918dbadad9fbf
Amirhossein <uplayggboy@gmail.com>
2023-04-26 06:30:00 +0330

    feat: added nodes and indicator routes to manage available nodes that are sharing files

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import logging
+import json
 from dotenv import load_dotenv
+from socket import socket
 
 from src.PathScanner import PathScanner
 from src.Server import Server
@@ -13,12 +15,39 @@ logging.basicConfig(
 
 load_dotenv()
 
+MIN_PORT_NUMBER = 5500
+MAX_PORT_NUMBER = 5600
+HOST = '127.0.0.1'
 
-port = int(os.getenv("PORT", 5000))
+port = None
+
+while True:
+    port = int(
+        input("Which port do you want to use should be between 5500 and 5600: "))
+
+    if MIN_PORT_NUMBER <= port <= MAX_PORT_NUMBER:
+        break
 
 router = Router()
 
 path = fr"{input('Which path of your computer do you want to share (path should be absolute): ')}"
+
+
+@router.get('/')
+def index(req: HttpRequest):
+    index_page_path = os.path.join(os.getcwd(), 'ui', 'dist', 'index.html')
+
+    with open(index_page_path, 'rb') as file:
+        return file.read()
+
+
+@router.get('/assets/:file')
+def assets(req: HttpRequest):
+    asset_path = os.path.join(
+        os.getcwd(), 'ui', 'dist', 'assets', req.get_param('file'))
+
+    with open(asset_path, 'rb') as file:
+        return file.read()
 
 
 @router.get('/list')
@@ -51,6 +80,43 @@ def send_file(req: HttpRequest):
 
     with open(path, 'rb') as file:
         return file.read()
+
+
+@router.get('/indicator')
+def node_indicator(req: HttpRequest):
+    return True
+
+
+@router.get('/nodes')
+def get_available_nodes(req: HttpRequest):
+    nodes = []
+
+    for port in range(MIN_PORT_NUMBER, MAX_PORT_NUMBER + 1):
+        client_socket = socket()
+
+        try:
+            client_socket.settimeout(0.1)
+            client_socket.connect((HOST, port))
+
+            client_socket.send(b"GET /indicator HTTP/1.1\r\n\r\n")
+
+            data = b""
+
+            while True:
+                d = client_socket.recv(4096)
+                data += d
+
+                if not d:
+                    break
+
+            if json.loads(data.decode().split("\r\n\r\n")[1]):
+                nodes.append(f"{HOST}:{port}")
+        except Exception as ex:
+            continue
+        finally:
+            client_socket.close()
+
+    return nodes
 
 
 server = Server(router)
