d2d45a7787af865b8cf4e02fdcc2e5f5c527ec40
Amirhossein <uplayggboy@gmail.com>
2023-05-03 18:00:59 +0330

    feat: applied new changes to main

--- main.py
+++ main.py
@@ -2,56 +2,53 @@ import os
 import logging
 
 from src.PathScanner import PathScanner
-from src.Node import Node
-from src.Router import Router
+from src.Server import Server
 from src.HttpRequest import HttpRequest
-from src.utils import fill_nodes, mime_detector
-from src.constants import MAX_PORT_NUMBER, MIN_PORT_NUMBER, NODES
-from src.types import RouterContext
+from src.HttpResponse import HttpResponse
+from src.HttpError import HttpError
+from src.utils import mime_detector
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
+port = int(
+    input("Which port do you want to use should be between 5500 and 5600: "))
 
-port = None
-
-while True:
-    port = int(
-        input("Which port do you want to use should be between 5500 and 5600: "))
-
-    if MIN_PORT_NUMBER <= port <= MAX_PORT_NUMBER:
-        break
-
-router = Router()
 
 path = fr"{input('Which path of your computer do you want to share (path should be absolute): ')}"
 
+server = Server(cors=True)
+
+router = server.router()
+
 
 @router.get('/')
-def index(req: HttpRequest, context: RouterContext):
+def index(req: HttpRequest, res: HttpResponse):
     index_page_path = os.path.join(os.getcwd(), 'ui', 'dist', 'index.html')
 
-    context["download"] = False
-
     with open(index_page_path, 'rb') as file:
-        context['mime'] = mime_detector(file.name)
+        mime = mime_detector(file.name)
+
+        res.set_header("Content-Type", mime)
+
         return file.read()
 
 
 @router.get('/assets/:file')
-def assets(req: HttpRequest, context: RouterContext):
+def assets(req: HttpRequest, res: HttpResponse):
     asset_path = os.path.join(
         os.getcwd(), 'ui', 'dist', 'assets', req.get_param('file'))
 
-    context["download"] = False
-
     with open(asset_path, 'rb') as file:
-        context['mime'] = mime_detector(file.name)
+        mime = mime_detector(file.name)
+
+        res.set_header("Content-Type", mime)
+
         return file.read()
 
 
 @router.get('/list')
-def get_dir_list(req: HttpRequest, _):
+def get_dir_list(req: HttpRequest, res: HttpResponse):
     prefix = req.get_query_param('prefix')
 
     real_path = path
@@ -65,12 +62,12 @@ def get_dir_list(req: HttpRequest, _):
 
 
 @router.get('/path')
-def get_path(req: HttpRequest, _):
+def get_path(req: HttpRequest, res: HttpResponse):
     return {"path": path}
 
 
 @router.get('/file')
-def send_file(req: HttpRequest, context: RouterContext):
+def send_file(req: HttpRequest, res: HttpResponse):
     path = req.get_query_param('path')
 
     if not path:
@@ -78,42 +75,16 @@ def send_file(req: HttpRequest, context: RouterContext):
 
     path = path[0]
 
-    context["download"] = True
-
     with open(path, 'rb') as file:
-        context['mime'] = mime_detector(file.name)
-        context["filename"] = file.name
+        mime = mime_detector(file.name)
+
+        res.set_header("Content-Type", mime)
+
+        res.set_header(
+            "content-disposition", f"attachment; filename={file.name}")
 
         return file.read()
 
 
-@router.get('/indicator')
-def node_indicator(req: HttpRequest, _):
-    return True
-
-
-@router.get('/nodes')
-def get_available_nodes(req: HttpRequest, _):
-    if len(NODES) > 0:
-        return NODES
-
-    NODES.clear()
-
-    fill_nodes(NODES)
-
-    return NODES
-
-
-@router.get('/refresh-nodes')
-def refresh_nodes(req: HttpRequest, _):
-    NODES.clear()
-
-    fill_nodes(NODES)
-
-    return NODES
-
-
-node = Node(router)
-
-node.listen(port)
-node.start()
+server.listen(port)
+server.start()
